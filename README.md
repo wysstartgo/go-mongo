cc4c7c4ffc81c6f327d29ae5dc134a2f95e63eda
wuys <yongsheng.wu@rtdream.cn>
2018-12-03 11:33:18 +0800

    go进行mongo数据迁移的工具

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go-mongo
-go语言写的mongo数据迁移工具，可用于集群
+> go语言写的mongo数据迁移工具，可用于集群
+
+
+> 运行方式：go-zm-dev.exe -config .\conf.json
---
+++ conf/conf.go
@@ -0,0 +1,30 @@
+package conf
+
+import (
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Conf struct {
+	SourceMongoUrl string	`json:"sourceMongoUrl"`
+	TargetMongoUrl string  `json:"targetMongoUrl"`
+	SourceCollection string `json:"sourceCollection"`
+	TargetCollection string `json:"targetCollection"`
+}
+
+var GConf *Conf
+
+func LoadConf(filename string) error {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	conf := Conf{}
+	err = json.Unmarshal(content, &conf)
+	if err != nil {
+		return err
+	}
+	GConf = &conf
+	return nil
+}
